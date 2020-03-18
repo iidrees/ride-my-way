9103174b40a46ecceb1cb82529f2963c98b162f9
iidrees <idreesibraheem@gmail.com>
2020-03-19 00:34:41 +0100

    new files added

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 import os
 
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 import settings
 
 app = Flask(__name__)
 
+db = SQLAlchemy(app)
+
+
 @app.route("/home")
 def hello_world():
     return "Hello World"
+
+
+
+@app.route("/profiles", methods=["GET", "POST"])
+def create_rides():
+    try:
+        first_name = request.form.get("first-name")
+        last_name = request.form.get("last-name")
+        ride_name = request.form.get("ride-name")
+
+        return "this is the name of the rider {} {}, and their ride {}".format(first_name, last_name, ride_name)
+    except Exception as e:
+        return(str(e))
+
 
 
 
