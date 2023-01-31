6f29ca5f3dd2c7a496ff7386eeb386c1cb93e376
Idrees Ibraheem <idreesibraheem@gmail.com>
2023-01-31 11:31:03 +0000

    - add models - create routes for creating and deleting users - add requirement file for package management

--- app.py
+++ app.py
@@ -3,30 +3,77 @@ import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-
 import settings
+# from config import Config
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
 
 db = SQLAlchemy(app)
+
+from api.models.user import User
 
 
 @app.route("/home")
 def hello_world():
-    return "Hello World"
+    user = User.query.filter().all()
+    print(user[0:])
+    return "Hello World {}".format(user[:])
 
 
-
-@app.route("/profiles", methods=["GET", "POST"])
-def create_rides():
+@app.route("/signup", methods=["POST"])
+def sign_up():
     try:
-        first_name = request.form.get("first-name")
-        last_name = request.form.get("last-name")
-        ride_name = request.form.get("ride-name")
+        # print(request)
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        ride_name = request.form.get("username")
 
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=ride_name
+        )
+
+        db.session.add(user)
+        db.session.commit()
         return "this is the name of the rider {} {}, and their ride {}".format(first_name, last_name, ride_name)
     except Exception as e:
         return(str(e))
+    
+
+@app.route("/profiles", methods=["GET", "POST","DELETE"])
+def create_rides():
+    if request.method == "POST": 
+        try:
+            # print(request.form.get)
+            first_name = request.form.get("first-name")
+            last_name = request.form.get("last-name")
+            ride_name = request.form.get("ride-name")
+            user = User(
+                first_name=first_name,
+                last_name=last_name,
+                username=ride_name
+            )
+
+            db.session.add(user)
+            db.session.commit()
+            return "this is the name of the rider {} {}, and their ride {}".format(first_name, last_name, ride_name)
+        except Exception as e:
+            return(str(e))
+    elif request.method == "DELETE":
+        first_name = request.form.get("first-name")
+        print(first_name)
+        try:
+            user = User.query.filter_by(username="abd").first()
+            print(">>>>>>>>",user)
+            db.session.delete(user)
+            db.session.commit()
+            return "User has been deleted"
+        except Exception as e:
+            return(str(e))
 
 
 
--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
